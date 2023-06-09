ff954074a42f5669a3d16dfb91c5daa4bc1c43ba
Gani Buer <pangeawelt@gmx.de>
2023-06-10 00:04:11 +0200

    junit

---
+++ src/test/java/tag07/C01_ClassWorkCheckBox.java
@@ -0,0 +1,60 @@
+package tag07;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class C01_ClassWorkCheckBox {
+    WebDriver driver;
+
+    //Vor jeder Test führt aus
+    @Before
+    public void setUp(){
+        WebDriverManager.chromedriver().setup();
+        driver= new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+    }
+
+    //Close
+    @After
+    public void tearDown() throws InterruptedException {
+        Thread.sleep(2000);
+        driver.quit();
+    }
+
+    @Test
+    public void test01(){
+        driver.get("https://the-internet.herokuapp.com/checkboxes");
+    WebElement checkbox1= driver.findElement(By.xpath("//input[@type='checkbox'][1]"));
+    WebElement checkbox2= driver.findElement(By.xpath("//input[@type='checkbox'][2]"));
+
+    //CheckBox klick
+        if (!checkbox1.isSelected()){
+            checkbox1.click();
+        }
+        if (!checkbox2.isSelected()){
+            checkbox1.click();
+        }
+
+     //   System.out.println(checkbox1.isSelected());
+       // System.out.println(checkbox2.isSelected());
+
+
+    }
+    ////input[type='checkbox'][1]
+
+
+    }
+
+    //https://the-internet.herokuapp.com/checkboxes
+
+
+
